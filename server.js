6cc1dd25eda038b92f3ca6054df2651b3d71abcf
duduialaAlexandru <alexandru24@live.com>
2018-12-10 23:04:20 +0200

    Another try

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+const port = process.env.PORT || 3000
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res)=>{
+    res.render('index.hbs', {
+        pageTitle: 'Main page',
+        pageName: 'home page'
+    })
+})
+
+app.get('/info', (req, res)=>{
+    res.render('info.hbs', {
+        pageTitle: 'Info page',
+        pageName: 'info page'
+    })
+})
+app.listen(port, ()=>{
+    console.log(`App running on ${port}`)
+})
