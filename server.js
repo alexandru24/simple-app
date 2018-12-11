bac0500d2dacf56cc376380aabe04c8e7e68c2ad
duduialaAlexandru <alexandru24@live.com>
2018-12-11 20:41:38 +0200

    Added user ip

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const hbs = require('hbs')
 const app = express()
 const port = process.env.PORT || 3000
+const ip = require('ip')
+
+let userIp = ip.address()
 
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs')
@@ -9,7 +12,8 @@ app.set('view engine', 'hbs')
 app.get('/', (req, res)=>{
     res.render('index.hbs', {
         pageTitle: 'Main page',
-        pageName: 'home page'
+        pageName: 'home page',
+        ipAddress: userIp
     })
 })
 
