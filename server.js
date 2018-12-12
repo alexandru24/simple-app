8907db76741ea41f1f110d41da7881283b3d7864
duduialaAlexandru <alexandru24@live.com>
2018-12-12 20:21:16 +0200

    Final version

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const hbs = require('hbs')
 const app = express()
+const axios = require('axios')
 const port = process.env.PORT || 3000
 
 
 hbs.registerPartials(__dirname+'/views/partials')
 app.set('view engine', 'hbs')
+let userLocation
+let userTemperature
+axios.get('https://jsonip.com/').then((response)=>{
+    let userIp = response.data.ip
+    let ipLookUp = `http://api.ipstack.com/${userIp}?access_key=9ef734509a141da137a63fe0404f122a`
+    return axios.get(ipLookUp)
+}).then((response)=>{
+    userLocation = response.data.city
+    let lat = response.data.latitude
+    let long = response.data.longitude
+    let weatherURL = `https://api.darksky.net/forecast/dadbe7d3d59ffb44a9ce0a9563c17f35/${lat},${long}`
+    return axios.get(weatherURL)
+}).then((response)=>{
+    let tempFahrenheit = response.data.currently.temperature
+    userTemperature = (tempFahrenheit - 32) * 5/9
+})
 
 app.get('/', (req, res)=>{
     res.render('index.hbs', {
         pageTitle: 'Main page',
-        pageName: 'home page'
+        pageName: 'home page',
+        userLocation: userLocation,
+        userTemperature: userTemperature.toFixed(0)
     })
 })
 
